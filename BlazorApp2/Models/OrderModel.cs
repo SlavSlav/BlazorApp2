7ce73c42dbc66bb2c215ee9735f542c6eef69549
Slava <spok452@gmail.com>
2023-10-11 03:35:15 +0300

    починаємо заповнювати

--- BlazorApp2/Models/OrderModel.cs
+++ BlazorApp2/Models/OrderModel.cs
@@ -3,7 +3,9 @@
     public class OrderModel
     {
         public Guid Id { get; set; }
+        public Guid UserId { get; set; }
         public UserModel User { get; set; }
+        public Guid GameId { get; set; }
         public GameModel Game { get; set; }
     }
 }
